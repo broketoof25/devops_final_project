66540de1c006fc1f8985f84ee4800e80703299ea
broketoof25 <andrew.s.lall@gmail.com>
2023-03-02 19:49:45 -0500

    third commit

--- app/dvdstore_db/tests.py
+++ app/dvdstore_db/tests.py
@@ -1,7 +1,10 @@
 from django.test import TestCase
 from django.urls import reverse
+from dvdstore_db.models import Movies
 import pytest
 # Create your tests here.
 def test_homepage_access():
     url = reverse('home')
     assert url == "/"
+
+
